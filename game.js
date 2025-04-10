659cdaa26cd84a02a6a0b3bb5ab5ea42e1d1f19f
TechShruti03 <gholapshruti03@gmail.com>
2025-04-10 20:10:39 +0530

    currentplayer variable declared

--- game.js
+++ game.js
@@ -2,3 +2,5 @@ const board = document.getElementById('board');
 const cells = document.querySelectorAll('.cell');
 const message = document.getElementById('message');
 const resetButton = document.getElementById('reset');
+
+let currentPlayer = 'X';
